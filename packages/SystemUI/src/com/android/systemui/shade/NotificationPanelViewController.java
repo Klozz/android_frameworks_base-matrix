Xtended-Mods/frameworks_base

SystemUI: Max Notif Panel Notification count [1/2]
SuperDroidBond: Sometimes entire notif panel gets filled with notification from
                different apps which makes us to miss out the important porns.

Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -393,6 +393,9 @@ public class NotificationPanelViewController extends PanelViewController {
     //Lockscreen Notifications
     private int mMaxKeyguardNotifConfig;
     private boolean mCustomMaxKeyguard;
+    //Notif Panel Notifications
+    private int mMaxNotifPanelNotifConfig;
+    private boolean mCustomMaxNotifPanel;
 
     /**
      * If set, the ongoing touch gesture might both trigger the expansion in {@link PanelView} and
@@ -1399,6 +1402,12 @@ public class NotificationPanelViewController extends PanelViewController {
             Settings.System.LOCK_SCREEN_CUSTOM_NOTIF, 0, UserHandle.USER_CURRENT) == 1;
         mMaxKeyguardNotifConfig = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
                  Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 3, UserHandle.USER_CURRENT);
+
+        mCustomMaxNotifPanel = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
+            Settings.System.NOTIF_PANEL_CUSTOM_NOTIF, 0, UserHandle.USER_CURRENT) == 1;
+        mMaxNotifPanelNotifConfig = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
+                 Settings.System.NOTIF_PANEL_MAX_NOTIF_CONFIG, 3, UserHandle.USER_CURRENT);
+
         if (mCustomMaxKeyguard) {
                 mMaxAllowedKeyguardNotifications = mMaxKeyguardNotifConfig;
         } else {
@@ -1415,8 +1424,12 @@ public class NotificationPanelViewController extends PanelViewController {
             mNotificationStackScrollLayoutController.setKeyguardBottomPaddingForDebug(
                     mKeyguardNotificationBottomPadding);
         } else {
-            // no max when not on the keyguard
-            mNotificationStackScrollLayoutController.setMaxDisplayedNotifications(-1);
+            if (mCustomMaxNotifPanel) {
+                mNotificationStackScrollLayoutController.setMaxDisplayedNotifications(mMaxNotifPanelNotifConfig);
+            } else {
+                // no max when not on the keyguard
+                mNotificationStackScrollLayoutController.setMaxDisplayedNotifications(-1);
+            }
             mNotificationStackScrollLayoutController.setKeyguardBottomPaddingForDebug(-1f);
         }
     }

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -4291,6 +4291,15 @@ public class CentralSurfacesImpl extends CoreStartable implements
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCK_SCREEN_CUSTOM_NOTIF),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.NOTIF_PANEL_CUSTOM_NOTIF),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.NOTIF_PANEL_MAX_NOTIF_CONFIG),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4333,6 +4342,9 @@ public class CentralSurfacesImpl extends CoreStartable implements
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.LOCK_SCREEN_CUSTOM_NOTIF)) ||
                 uri.equals(Settings.System.getUriFor(Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG))) {
                 setMaxKeyguardNotifConfig();
+            } else if (uri.equals(Settings.System.getUriFor(Settings.System.NOTIF_PANEL_CUSTOM_NOTIF)) ||
+                uri.equals(Settings.System.getUriFor(Settings.System.NOTIF_PANEL_MAX_NOTIF_CONFIG))) {
+		setMaxNotifPanelNotifConfig();
             }
         }
 
@@ -4348,6 +4360,7 @@ public class CentralSurfacesImpl extends CoreStartable implements
             setPulseOnNewTracks();
             setUseReTicker();
             setMaxKeyguardNotifConfig();
+            setMaxNotifPanelNotifConfig();
         }
     }
 
@@ -4445,6 +4458,16 @@ public class CentralSurfacesImpl extends CoreStartable implements
                  Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 3, UserHandle.USER_CURRENT);
 
         mNotificationPanelViewController.updateMaxDisplayedNotifications(customMaxKeyguard);
+    }
+
+    private void setMaxNotifPanelNotifConfig() {
+        boolean customMaxNotifPanel = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.NOTIF_PANEL_CUSTOM_NOTIF, 0, UserHandle.USER_CURRENT) == 1;
+
+        int maxNotifPanelNotifConfig = Settings.System.getIntForUser(mContext.getContentResolver(),
+                 Settings.System.NOTIF_PANEL_MAX_NOTIF_CONFIG, 3, UserHandle.USER_CURRENT);
+
+        mNotificationPanelViewController.updateMaxDisplayedNotifications(customMaxNotifPanel);
     }
 
     private final BroadcastReceiver mBannerActionBroadcastReceiver = new BroadcastReceiver() {

Klozz/android_frameworks_base-matrix

Signed-off-by: mukesh22584 <mks22584@gmail.com>

packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -435,6 +435,9 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     //Lockscreen Notifications
     private int mMaxKeyguardNotifConfig;
     private boolean mCustomMaxKeyguard;
+    //Notif Panel Notifications
+    private int mMaxNotifPanelNotifConfig;
+    private boolean mCustomMaxNotifPanel;
 
     private boolean mPanelExpanded;
 
@@ -1568,6 +1571,14 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             Settings.System.LOCK_SCREEN_CUSTOM_NOTIF, 0, UserHandle.USER_CURRENT) == 1;
         mMaxKeyguardNotifConfig = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
                  Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 3, UserHandle.USER_CURRENT);
+
+        mCustomMaxNotifPanel = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
+            Settings.System.NOTIF_PANEL_CUSTOM_NOTIF, 0, UserHandle.USER_CURRENT) == 1;
+        mMaxNotifPanelNotifConfig = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
+                 Settings.System.NOTIF_PANEL_MAX_NOTIF_CONFIG, 3, UserHandle.USER_CURRENT);
+
+        int newMaxNotifPanelNotifConfig = (mMaxNotifPanelNotifConfig + 1);
+
         if (mCustomMaxKeyguard) {
                 mMaxAllowedKeyguardNotifications = mMaxKeyguardNotifConfig;
         } else {
@@ -1584,8 +1595,12 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             mNotificationStackScrollLayoutController.setKeyguardBottomPaddingForDebug(
                     mKeyguardNotificationBottomPadding);
         } else {
-            // no max when not on the keyguard
-            mNotificationStackScrollLayoutController.setMaxDisplayedNotifications(-1);
+            if (mCustomMaxNotifPanel) {
+                mNotificationStackScrollLayoutController.setMaxDisplayedNotifications(newMaxNotifPanelNotifConfig);
+            } else {
+                // no max when not on the keyguard
+                mNotificationStackScrollLayoutController.setMaxDisplayedNotifications(-1);
+            }
             mNotificationStackScrollLayoutController.setKeyguardBottomPaddingForDebug(-1f);
         }
     }

packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -3566,6 +3566,12 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.NOTIF_PANEL_CUSTOM_NOTIF),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.NOTIF_PANEL_MAX_NOTIF_CONFIG),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -3574,11 +3580,15 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             if (uri.equals(Settings.System.getUriFor(Settings.System.LOCK_SCREEN_CUSTOM_NOTIF)) ||
                 uri.equals(Settings.System.getUriFor(Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG))) {
                 setMaxKeyguardNotifConfig();
+            } else if (uri.equals(Settings.System.getUriFor(Settings.System.NOTIF_PANEL_CUSTOM_NOTIF)) ||
+                uri.equals(Settings.System.getUriFor(Settings.System.NOTIF_PANEL_MAX_NOTIF_CONFIG))) {
+		setMaxNotifPanelNotifConfig();
             }
         }
 
         public void update() {
             setMaxKeyguardNotifConfig();
+            setMaxNotifPanelNotifConfig();
         }
     }
 
@@ -3590,6 +3600,16 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
                  Settings.System.LOCKSCREEN_MAX_NOTIF_CONFIG, 3, UserHandle.USER_CURRENT);
 
         mNewNotificationPanelViewController.updateMaxDisplayedNotifications(customMaxKeyguard);
+    }
+
+    private void setMaxNotifPanelNotifConfig() {
+        boolean customMaxNotifPanel = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.NOTIF_PANEL_CUSTOM_NOTIF, 0, UserHandle.USER_CURRENT) == 1;
+
+        int maxNotifPanelNotifConfig = Settings.System.getIntForUser(mContext.getContentResolver(),
+                 Settings.System.NOTIF_PANEL_MAX_NOTIF_CONFIG, 3, UserHandle.USER_CURRENT);
+
+        mNewNotificationPanelViewController.updateMaxDisplayedNotifications(customMaxNotifPanel);
     }
 
     private final BroadcastReceiver mBannerActionBroadcastReceiver = new BroadcastReceiver() {